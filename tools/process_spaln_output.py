61fb80772a2ea1a5b6eb008d6a9045286ead7237
Peter van Heusden <pvh@sanbi.ac.za>
2020-09-16 16:43:46 +0200

    Extend process_spaln_output.py to do sorting and sequence-region

--- tools/process_spaln_output.py
+++ tools/process_spaln_output.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, division
+from operator import itemgetter
 import re
 import sys
 
@@ -31,34 +32,58 @@ def fix(attrs, region_name, number):
 
 output_filename = "spaln_out.gff3"
 output_file = open(output_filename, "w")
+print('##gff-version\t3', file=output_file)
 input_filenames = sys.argv[1:]
 seen_header = False
 gene_number = 0
+annotation_parts = {}
+start_position = {}
+end_position = {}
+max_end = 0
 for input_filename in input_filenames:
     for line in open(input_filename):
-        if line.startswith("##sequence-region"):
+        if line.startswith('##sequence-region'):
             parts = line.split()
             orig_region_name = parts[1]
             location_parts = parts[1].split(":")
             region_name = location_parts[0]
             start = int(location_parts[1].split("-")[0])
-            if not seen_header:
-                pass
-                # sequence-region must have a start and end coordinate - we don't now these
-                # until we have seen the maximal output position, so leave this out
-                # TODO: write script that samples all files, determines sort order
-                # and uses this "global overview" to insert sequence-region line
-                # print('##sequence-region\t{}'.format(region_name), file=output_file)
-            seen_header = True
-        elif line.startswith("#"):
-            if not seen_header:
-                print(line, end="", file=output_file)
-            else:
-                continue
-        else:
+            if region_name not in annotation_parts:
+                annotation_parts[region_name] = []
+            annotation_parts[region_name].append((start, region_name, input_filename))
+        elif not line.startswith('#'):
             fields = line.rstrip().split("\t")
-            type = fields[2]
-            if type == "gene":
+            seq_type = fields[2]
+            if seq_type == 'gene':
+                # end is fields[4]
+                max_end = int(fields[4]) + start
+    else:
+        if region_name not in end_position or end_position[region_name] < max_end:
+            end_position[region_name] = max_end
+        if region_name not in start_position or start_position[region_name] > start:
+            start_position[region_name] = start
+
+for annotation_part, locations in sorted(annotation_parts.items(), key=itemgetter(1)):
+    # locations is a list of tupes (starting_position, region_name, filename)
+    sequence_region_added = False
+    for start, region_name, input_filename in sorted(locations, key=itemgetter(0)):
+        if not sequence_region_added:
+            assert start == start_position[region_name], "mismatch between this start position {} and computed start position {} for {}".format(start, start_position[region_name], region_name)
+            # started a new contig, add a sequence region header that describes the region under annotation
+            print('##sequence-region\t{} {} {}'.format(region_name, start, end_position[region_name]), file=output_file)
+            sequence_region_added = True
+
+        for line in open(input_filename):
+            if line.startswith("##sequence-region") or line.startswith('##gff-version'):
+                continue
+            elif line.startswith('##FASTA'):
+                break
+            elif line.startswith('##'):
+                print('Warning: unexpected directive {}'.format(line.strip()), file=output_file)
+                continue
+            fields = line.rstrip().split("\t")
+            seq_type = fields[2]
+            if seq_type == "gene":
                 gene_number += 1
             fields[0] = region_name
             fields[3] = str(int(fields[3]) + start)
